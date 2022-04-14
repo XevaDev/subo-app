810e99381b48139099dc41c18626c624ae952816
XevaDev <xevaltan@gmail.com>
2022-04-14 16:57:17 +0200

    m

--- Subo Client/src/App.svelte
+++ Subo Client/src/App.svelte
@@ -1,10 +1,18 @@
 <script>
+	import Navbar from "./Navbar.svelte";	
 	export let name;
+
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>This is the start of the Subo Client</p>
+	<Navbar></Navbar>
+	<div align="center">
+		<h1>Hello {name}!</h1>
+		<p>This is the start of the Subo Client</p>
+
+	</div>
+
+
 </main>
 
 <style>
--- Subo Client/src/Navbar.svelte
+++ Subo Client/src/Navbar.svelte
@@ -1,7 +1,36 @@
-<div>
+<script>
+</script>
+
+<div class="nav">
     <div class="navbtns">
         <a class="navbtn" type="button" href="/">Menu</a>
         <a class="navbtn" type="button" href="/">Categories</a>
         <a class="navbtn" type="button" href="/">Discover</a>
     </div>
 </div>
+
+<style lang="scss">
+    $color-a: rgb(161, 23, 23);
+    $color-b: rgb(153, 58, 58);
+    .nav {
+        background-color: $color-b;
+        height: 38px;
+        width: 100%;
+        text-align: center;
+        display: block;
+        border: 1px solid black;
+        .navbtns {
+            
+            padding-top: 8px;
+            .navbtn {
+                background-color: $color-a;
+                margin: 5px;
+                padding: 5px;
+                color: white;
+                font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+                margin-right: 10px;
+                border-radius: 20%;
+            }
+        }
+    }
+</style>
