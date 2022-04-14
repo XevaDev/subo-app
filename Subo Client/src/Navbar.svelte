5d369f9ef4202e296039f345850c20558cb69997
XevaDev <xevaltan@gmail.com>
2022-04-14 16:19:54 +0200

    m

---
+++ Subo Client/src/Navbar.svelte
@@ -0,0 +1,7 @@
+<div>
+    <div class="navbtns">
+        <a class="navbtn" type="button" href="/">Menu</a>
+        <a class="navbtn" type="button" href="/">Categories</a>
+        <a class="navbtn" type="button" href="/">Discover</a>
+    </div>
+</div>
