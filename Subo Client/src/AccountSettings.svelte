6e5bcea3a2ec32b74edfccc9e82cd752c47ec229
XevaDev <xevaltan@gmail.com>
2022-05-20 18:38:10 +0200

    work

--- Subo Client/src/AccountSettings.svelte
+++ Subo Client/src/AccountSettings.svelte
@@ -1,18 +1,55 @@
 <script lang="ts">
   import { onMount } from "svelte";
+  import App from "./App.svelte";
+  import { checkExist } from "./checkExist";
+  import { getClientInfo } from "./getClientInfo";
   import UserIcon from "./UserIcon.svelte";
   import UserProfile from "./UserProfile.svelte";
 
-  export let settings: {
-    username: string;
-    profileLink: string;
-    imageSource: string;
-  };
+  let vars = async function () {
+    let t = await checkExist();
+    let settings: {
+      imageSource: string;
+      username: string;
+      profileLink: string;
+    };
 
-  onMount(() => {});
+    if (t === true) {
+      let g = await getClientInfo(localStorage.token, "http://localhost:2000");
+
+      settings = {
+        imageSource: g.avatar,
+        username: g.username,
+        profileLink: "/",
+      };
+    }
+
+    return { settings: settings, t: t };
+  };
 </script>
 
 <div>
-  <h1>Settings</h1>
-  <h2>Logged in as {settings.username}</h2>
+  {#await vars() then v}
+    <h1>
+      <UserIcon
+        profileLink={v.settings.profileLink}
+        src={v.settings.imageSource}
+        username={v.settings.username}
+      /> Account Settings: {v.settings.username}
+    </h1>
+    <h2>Change username:</h2>
+    <div class="chname">
+      <div class="input-group mb-3">
+        <button type="button" class="btn btn-primary">Set</button>
+        <input
+          type="text"
+          class="form-control"
+          aria-label="Sizing example input"
+          aria-describedby="inputGroup-sizing-default"
+          placeholder="new username..."
+        />
+      </div>
+    </div>
+  {/await}
+  <br />
 </div>
--- Subo Client/src/App.svelte
+++ Subo Client/src/App.svelte
@@ -5,19 +5,17 @@
   import Navbar from "./Navbar.svelte";
   import Page from "./Page.svelte";
 
-  let page: string | undefined = undefined;
+  let page: string | null = null;
 
   onMount(() => {
     page = _GET("page");
-    document.title = "Subo App";
-    document.body.style.backgroundColor = "#E6D4D0";
   });
 </script>
 
 <main>
   <Navbar />
   <br />
-  {#if typeof page === "undefined"}
+  {#if page === null}
     <Page page={1} />
   {:else}
     <Page page={parseInt(page)} />
